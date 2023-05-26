2d829403ee6b001606ed13d77f0650edd6fa8728
Alberico Fagundes <alberico@fagundes.live>
2023-05-26 00:42:43 -0300

    criado rotina img

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+const { Client, MessageMedia } = require("whatsapp-web.js");
 const express = require("express");
 const app = express();
+const axios = require("axios");
+const {
+  Sticker,
+  createSticker,
+  StickerTypes,
+} = require("wa-sticker-formatter");
 
 app.use(express.json());
 
 app.get("/sticker", async function (req, res) {
   try {
-    const currentDate = new Date();
     const url = req.body.url;
 
-    // Aqui você pode adicionar o código adicional que precisa ser executado com async-await
-
-    res.json({
-      currentDate: currentDate,
-      url: url,
+    const { data } = await axios.get(url, { responseType: "arraybuffer" });
+    const buffer = await createSticker(data, {
+      pack: "My Pack",
+      author: "Alberico Fagundes",
+      type: StickerTypes.FULL,
+      id: "12345",
+      quality: 50,
+      background: "#000000",
     });
+    console.log("buffer", buffer);
+
+    res.setHeader("Content-Type", "image/webp");
+    res.send(buffer);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
